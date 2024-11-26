80028cb6c8f089ab39da555adadca3615a9b290f
Sindhu <sindhukrish0009@gmail.com>
2024-11-26 22:00:50 +0530

    added one file Tc_frm_locrep and modified NEWFILEADDED

---
+++ src/test/java/selgit1/Tc_frm_locrep.java
@@ -0,0 +1,13 @@
+package selgit1;
+
+public class Tc_frm_locrep {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		"Ths is file created in local local wrk space"
+
+	}
+
+}
--- src/test/java/selgit1/newfileadded.java
+++ src/test/java/selgit1/newfileadded.java
@@ -1,1 +1,2 @@
 "I have added new file , pls review code in your local repo"
+"I have added newly now"
